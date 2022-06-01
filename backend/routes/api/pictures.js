8bc57a2fd8adadd530b268d6f7fd341d53cbb424
Dylan <d.peate63@gmail.com>
2022-06-01 16:10:10 -0400

    debug

--- backend/routes/api/pictures.js
+++ backend/routes/api/pictures.js
@@ -9,4 +9,26 @@ router.get('/', asyncHandler(async function (req, res) {
     return res.json(pictures)
 }))
 
+//open photo
+router.get('/:id', asyncHandler(async function (req, res) {
+    const pictureId = parseInt(req.params.id, 10);
+    const picture = await db.Picture.findOne({
+        where: { id: pictureId }
+    })
+    return res.json(picture)
+}))
+
+//edit photo
+router.put('/:id', asyncHandler(async function (req, res) {
+
+}))
+//delete photo
+router.delete('/:id', asyncHandler(async function (req, res) {
+
+}))
+//upload photo
+router.post('/', asyncHandler(async function (req, res) {
+
+}))
+
 module.exports = router;
